a90fa5396028fcf42915e0562809cb477bb7ac73
Pablo Lalloni <plalloni@gmail.com>
2011-05-27 08:30:09 -0300

    DSL improvements

---
+++ src/main/scala/org/retistruen/dsl.scala
@@ -0,0 +1,39 @@
+package org.retistruen
+
+class Model {
+
+  type E[T] = Emitter[T]
+  type R[T] = Receiver[T]
+  type RE[T] = Receiver[T] with Emitter[T]
+  type ER[T] = E[T] ⇒ R[T]
+  type ERE[T] = E[T] ⇒ RE[T]
+
+  def source[T](name: String): E[T] = new SourceEmitter[T](name)
+
+  def rec[T, R]: ER[T] = { e ⇒ new RecorderReceiver[T](name(e, "rec")) }
+
+  def max[T: Ordering]: ERE[T] = { e ⇒ new Max[T](name(e, "max")) }
+
+  def max[T: Ordering](size: Int): ERE[T] = { e ⇒ new BufferedMax[T](name(e, "max" + size), size) }
+
+  private def name(previous: E[_], tag: String) = previous.name + "." + tag
+
+  implicit def emitterConnector[T](emitter: E[T]) = new Connector[T](emitter)
+
+  class Connector[T](emitter: E[T]) {
+    def -->(builder: ERE[T]): E[T] = emitter >> builder(emitter)
+    def --|(builder: ERE[T]): E[T] = emitter >> builder(emitter)
+  }
+
+}
+
+object Sensors extends Model {
+
+  val s1 = source[BigDecimal]("s1")
+  val s2 = source[BigDecimal]("s2")
+
+  s1 --> max --> rec
+
+  s1 --> mean --> max(10) --> rec
+
+}
