dca4c2a4722871c73904afebc1b9e9f13be7d88f
Pablo Lalloni <plalloni@gmail.com>
2011-06-02 16:14:39 -0300

    Added FrequencySurvey and Spec

---
+++ src/main/scala/org/retistruen/survey/FrequencySurvey.scala
@@ -0,0 +1,62 @@
+/*
+ * Author: Pablo Lalloni <plalloni@gmail.com>
+ * Created: 02/06/2011 11:14:35
+ */
+package org.retistruen.survey
+
+import org.joda.time.{ ReadablePeriod, Period, Instant }
+import org.retistruen.{ Source, Datum, Tagging }
+import scala.actors.{ TIMEOUT, Actor }
+import scala.collection.mutable.Publisher
+
+case object Stop
+
+trait Chronometric {
+  val instant = new Instant
+}
+
+case class Tick() extends Chronometric
+
+class TickTimer(target: Actor, period: ReadablePeriod) extends Actor {
+
+  private val millis = period.toPeriod.toStandardDuration.getMillis
+
+  def act = loop {
+    reactWithin(millis) {
+      case TIMEOUT ⇒ target ! new Tick
+      case Stop    ⇒ exit
+    }
+  }
+
+  def stop = this ! Stop
+
+  start
+
+}
+
+case class Beat() extends Chronometric
+
+class FrequencySurvey(source: Source[Int], period: ReadablePeriod, tagging: Option[Tagging] = None) extends Actor {
+
+  private val duration = period.toPeriod.toStandardDuration
+
+  private var beats: Seq[Beat] = Seq.empty
+
+  private val timer = new TickTimer(this, period)
+
+  def act = loop {
+    react {
+      case tick: Tick ⇒
+        beats = beats.filter(_.instant isAfter tick.instant.minus(duration))
+        source << Datum(beats.count(_.instant isBefore tick.instant), tick.instant, tagging)
+      case beat: Beat ⇒
+        beats :+= beat
+    }
+  }
+
+  def beat =
+    this ! new Beat
+
+  start
+
+}
---
+++ src/test/scala/org/retistruen/survey/FrequencySurveySpec.scala
@@ -0,0 +1,73 @@
+package org.retistruen.survey
+
+import org.joda.time.Seconds._
+import org.retistruen.instrument.{ RecordingReceiver, SourceEmitter }
+import org.retistruen.{ Emitter, Datum, Receiver }
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Spec
+import org.mockito.Mockito._
+import scala.collection.mutable.Buffer
+import org.joda.time.Duration
+
+class FrequencySurveySpec extends Spec with ShouldMatchers {
+
+  describe("A 2 seconds FrequencySurvey") {
+    describe("when receiving 100 beats in less than 2 seconds") {
+      it("should emit 100 in the next tick") {
+
+        val source = new SourceEmitter[Int]("s")
+        val receiver = new RecordingReceiver[Int]("r")
+
+        source >> receiver
+
+        val freq = new FrequencySurvey(source, seconds(2))
+
+        Thread.sleep(1000)
+        1.to(100).foreach(_ ⇒ freq.beat)
+        Thread.sleep(2000)
+
+        receiver.dataValues.headOption should equal(Some(100))
+
+      }
+    }
+  }
+
+  describe("A 1 second FrequencySurvey") {
+    describe("when sent 50 beats every 100 ms") {
+      it("should emit frequencies once a second and last should be 10") {
+        val source = new SourceEmitter[Int]("s")
+
+        val emitted: Buffer[Datum[Int]] = Buffer.empty
+        var lastValue: Int = 0
+
+        source >> new Receiver[Int] {
+          val name = "r"
+          def receive(e: Emitter[Int], d: Datum[Int]) = {
+            lastValue = d.value
+            emitted append d
+            println(d)
+          }
+        }
+
+        val survey = new FrequencySurvey(source, seconds(1))
+
+        1.to(100).foreach { _ ⇒
+          Thread.sleep(100)
+          survey.beat
+        }
+
+        lastValue should equal(10)
+
+        val instants = emitted.map(_.created)
+        val diffs = (instants.dropRight(1) zip instants.drop(1))
+          .map(p ⇒ new Duration(p._1, p._2).getMillis)
+
+        diffs.foreach { d ⇒
+          d should (be < (1100l) and be > (900l))
+        }
+
+      }
+    }
+  }
+
+}
