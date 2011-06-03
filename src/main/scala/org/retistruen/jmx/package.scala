9674efd220800e9f836bca5f841aea3a51307afc
Pablo Lalloni <plalloni@gmail.com>
2011-06-03 19:14:28 -0300

    Added initial JMX support

---
+++ src/main/scala/org/retistruen/jmx/package.scala
@@ -0,0 +1,23 @@
+package org.retistruen
+
+import javax.management.{ MBeanServer, MBeanServerFactory }
+import scala.collection.JavaConversions._
+import java.lang.management.ManagementFactory
+
+/**
+ * This package contains plugins for registering retistruen instruments on JMX.
+ * See [[org.retistruen.jmx.MBeanRegistering]] for futher detail and usage.
+ */
+package object jmx {
+
+  /** Returns the set of MBeanServer with matching default domain name. */
+  def mbeanServers(domain: String): Seq[MBeanServer] =
+    MBeanServerFactory.findMBeanServer(null).filter(_.getDefaultDomain == domain)
+
+  /** Returns the first MBeanServer matching default domain name or none if not found. */
+  def mbeanServer(domain: String): Option[MBeanServer] = mbeanServers(domain).headOption
+
+  def bestMBeanServer(domain: String): MBeanServer =
+    mbeanServer(domain).getOrElse(ManagementFactory.getPlatformMBeanServer)
+
+}
