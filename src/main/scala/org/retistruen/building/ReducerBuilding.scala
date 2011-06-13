13751ccb202b951336ad219f585a15d241848a15
Pablo Lalloni <plalloni@gmail.com>
2011-06-13 00:10:55 -0300

    Implemented more descriptive statistics reduce functions
    - Open
    - Close
    - Range
    - Median
    - Percentile
    - Mode

--- src/main/scala/org/retistruen/building/ReducerBuilding.scala
+++ src/main/scala/org/retistruen/building/ReducerBuilding.scala
@@ -6,7 +6,7 @@ import reduce._
 
 trait ReducerBuilding {
 
-  this: Building ⇒
+  this: BuildingInfrastructure ⇒
 
   protected def reduce[T, R](name: String, function: Seq[T] ⇒ R) =
     register { e: Emitter[Seq[Datum[T]]] ⇒ new Reducer[T, R](receiverName(e, "reduce", name), { seq: Seq[Datum[T]] ⇒ Datum(function(seq.map(_.value))) }) }
@@ -14,7 +14,7 @@ trait ReducerBuilding {
   protected def reduce[T, R] = new ReducerBuilding[T, R]
 
   protected class ReducerBuilding[T, R] {
-    private def builder[T, R](function: ReduceFunction[T, R]) = { e: Emitter[Seq[Datum[T]]] ⇒ new Reducer[T, R](receiverName(e, "reduce", function), function) }
+    private[ReducerBuilding] def builder[T, R](function: ReduceFunction[T, R], args: Any*) = { e: Emitter[Seq[Datum[T]]] ⇒ new Reducer[T, R](receiverName(e, "reduce", (function +: args): _*), function) }
     def sum[T: Numeric] = register(builder(new Sum[T]))
     def product[T: Numeric] = register(builder(new Product[T]))
     def max[T: Ordering] = register(builder(new Max[T]))
@@ -24,6 +24,12 @@ trait ReducerBuilding {
     def stddev[T: Fractional] = register(builder(new StandardDeviation[T]))
     def skewness[T: Fractional] = register(builder(new Skewness[T]))
     def kurtosis[T: Fractional] = register(builder(new Kurtosis[T]))
+    def open[T] = register(builder(new Open[T]))
+    def close[T] = register(builder(new Close[T]))
+    def mode[T] = register(builder(new Mode[T]))
+    def median[T: Fractional] = register(builder(new Median[T]))
+    def percentile[T: Fractional](n: Int) = register(builder(new Percentile[T](n), n))
+    def range[T: Numeric] = register(builder(new Range[T]))
   }
 
 }
--- src/main/scala/org/retistruen/instrument/reduce/ReduceFunction.scala
+++ src/main/scala/org/retistruen/instrument/reduce/ReduceFunction.scala
@@ -5,26 +5,25 @@ import org.retistruen.Named
 
 package object algo {
 
-  def pow[T: Numeric](base: T, exponent: Int) = {
-    val t = implicitly[Numeric[T]]
+  def intpow[T: Numeric](base: T, exponent: Int): T = {
+    assert(exponent >= 0, "Only zero and positives are supported as exponent")
+    val t = implicitly[Numeric[T]]; import t._
     var product = t.one
-    for (i ← 1 to exponent) product = t.times(product, base)
+    for (i ← 1 to exponent) product = product * base
     product
   }
 
-  def sum[T: Numeric](seq: Seq[T]) = seq.sum
-
-  def size(seq: Seq[_]) = seq.size
-
   def mean[T: Fractional](seq: Seq[T]) = {
-    val t = implicitly[Fractional[T]]
-    t.div(sum(seq), t.fromInt(size(seq)))
+    assert(!seq.isEmpty, "Can not calculate the mean of an empty sequence")
+    val t = implicitly[Fractional[T]]; import t._
+    seq.sum / t.fromInt(seq.size)
   }
 
   def moment[T: Fractional](k: Int)(seq: Seq[T]): T = {
-    val t = implicitly[Fractional[T]]
+    assert(!seq.isEmpty, "Can not calculate a moment of an empty sequence")
+    val t = implicitly[Fractional[T]]; import t._
     val u = mean(seq)
-    seq.map(e ⇒ pow(t.minus(e, u), k)).sum
+    seq.map(e ⇒ intpow(e - u, k)).sum
   }
 
   def variance[T: Fractional](seq: Seq[T]) = moment(2)(seq)
@@ -35,6 +34,48 @@ package object algo {
 
   def standardDeviation[T: Fractional](seq: Seq[T]) =
     math.sqrt(implicitly[Fractional[T]].toDouble(variance(seq)))
+
+  def percentRank[T: Fractional](size: Int)(o: Int): T = {
+    assert(size > 0, "Size must be greater than zero to calculate percent rank")
+    assert(o >= 0 && o <= size, "Ordinal must be between 0 and size (inclusive): " + o)
+    val t = implicitly[Fractional[T]]; import t._
+    (t.fromInt(100) / t.fromInt(size)) * (t.fromInt(o) - (t.one / t.fromInt(2)))
+  }
+
+  def roundedPercentile[T: Ordering](p: Int)(seq: Seq[T]): T = {
+    assert(!seq.isEmpty, "Can not calculate the mean of an empty sequence")
+    assert(p >= 0 && p <= 100, "Percentile must be between 0 and 100 (inclusive): " + p)
+    val size = seq.size
+    val value = seq.sorted
+    val ordinalRank = math.round((p / 100.0) * size + 0.5).toInt
+    value(ordinalRank)
+  }
+
+  def interpolatedPercentile[T: Fractional](p: Int)(seq: Seq[T]): T = {
+    assert(!seq.isEmpty, "Can not calculate the mean of an empty sequence")
+    assert(p >= 0 && p <= 100, "Percentile must be between 0 and 100 (inclusive): " + p)
+    val t = implicitly[Fractional[T]]; import t._
+    val value = seq.sorted
+    val size = value.size
+    val baseOrdinalRank = math.floor((p / 100.0) * size).toInt
+    val nextOrdinalRank = baseOrdinalRank + 1
+    val rank = percentRank[T](size)(_)
+    val baseValue = value(baseOrdinalRank)
+    val nextValue = value(nextOrdinalRank)
+    val baseRank = rank(baseOrdinalRank)
+    val nextRank = rank(nextOrdinalRank)
+    baseValue +
+      t.fromInt(size) * (nextRank - baseRank) /
+      t.fromInt(100) * (nextValue - baseValue)
+  }
+
+  def interpolatedQuartile[T: Fractional](q: Int)(values: Seq[T]) = {
+    assert(q >= 0 && q <= 4, "Quartile must be between 0 and 4 (inclusive): " + q)
+    interpolatedPercentile[T](q * 25)(values)
+  }
+
+  def interpolatedMedian[T: Fractional](values: Seq[T]) =
+    interpolatedPercentile[T](50)(values)
 
 }
 
@@ -92,4 +133,47 @@ class Product[T: Numeric] extends ReduceFunction[T, T] {
   val name = "product"
   def apply(data: Seq[Datum[T]]): Datum[T] =
     Datum(data.map(_.value).product)
+}
+
+class Open[T] extends ReduceFunction[T, T] {
+  val name = "open"
+  def apply(data: Seq[Datum[T]]): Datum[T] =
+    Datum(data.head.value)
+}
+
+class Close[T] extends ReduceFunction[T, T] {
+  val name = "close"
+  def apply(data: Seq[Datum[T]]): Datum[T] =
+    Datum(data.last.value)
+}
+
+class Mode[T] extends ReduceFunction[T, T] {
+  val name = "mode"
+  def apply(data: Seq[Datum[T]]): Datum[T] =
+    Datum(data.map(_.value).groupBy(identity).maxBy(_._2.size)._1)
+}
+
+class Median[T: Fractional] extends ReduceFunction[T, T] {
+  val name = "median"
+  val t = implicitly[Fractional[T]]; import t._
+  def apply(data: Seq[Datum[T]]): Datum[T] =
+    Datum(algo.interpolatedMedian(data.map(_.value)))
+}
+
+class Range[T: Numeric] extends ReduceFunction[T, T] {
+  val name = "range"
+  val t = implicitly[Numeric[T]]; import t._
+  def apply(data: Seq[Datum[T]]): Datum[T] = {
+    val values = data.map(_.value)
+    Datum(values.max - values.min)
+  }
+}
+
+class Percentile[T: Fractional](p: Int) extends ReduceFunction[T, T] {
+  val name = "percentile"
+  val t = implicitly[Numeric[T]]; import t._
+  def apply(data: Seq[Datum[T]]): Datum[T] = {
+    val values = data.map(_.value)
+    Datum(algo.interpolatedPercentile(p)(values))
+  }
 }
