1a857149c40346cef0508711eb90479f9ffc8750
Pablo Lalloni <plalloni@gmail.com>
2011-06-13 00:09:01 -0300

    Added OpenSourceEmitter

--- src/main/scala/org/retistruen/instrument/SourceEmitter.scala
+++ src/main/scala/org/retistruen/instrument/SourceEmitter.scala
@@ -2,6 +2,13 @@ package org.retistruen.instrument
 
 import org.retistruen.CachingEmitter
 import org.retistruen.Source
+import org.retistruen.OpenSource
+import org.retistruen.ReadableFromString
 
-/** A simple [[org.retistruen.Emitter]] that to be used as entry point */
-class SourceEmitter[@specialized T](val name: String) extends Source[T] with CachingEmitter[T]
+/** A simple [[org.retistruen.Source]] [[org.retistruen.Emitter]] to be used as entry point */
+class SourceEmitter[T](val name: String)
+  extends Source[T] with CachingEmitter[T]
+
+/** An [[org.retistruen.OpenSource]] [[org.retistruen.Emitter]] to be used as entry point */
+class OpenSourceEmitter[T: ReadableFromString](val name: String)
+  extends OpenSource[T] with CachingEmitter[T]
