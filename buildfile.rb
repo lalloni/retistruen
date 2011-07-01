40c81fcaf4a44a239da863ab5945fa9987bfcefa
Pablo Lalloni <plalloni@gmail.com>
2011-07-01 12:03:41 -0300

    Added grizzled-slf4j dependency

--- buildfile.rb
+++ buildfile.rb
@@ -10,11 +10,11 @@ THIS_VERSION = "0.3-SNAPSHOT"
 repositories.remote << "http://www.ibiblio.org/maven2/"
 repositories.remote << "http://akka.io/repository"
 repositories.release_to[:url] =
-  if THIS_VERSION =~ /-SNAPSHOT$/
-    "http://artifactsddit.afip.gov.ar/nexus/content/repositories/external-snapshots"
-  else
-    "http://artifactsddit.afip.gov.ar/nexus/content/repositories/external"
-  end
+if THIS_VERSION =~ /-SNAPSHOT$/
+  "http://artifactsddit.afip.gov.ar/nexus/content/repositories/external-snapshots"
+else
+  "http://artifactsddit.afip.gov.ar/nexus/content/repositories/external"
+end
 
 # Compile Dependencies
 JODA_TIME = "joda-time:joda-time:jar:1.6.2"
@@ -22,6 +22,11 @@ JUNG = group("jung-api", "jung-graph-impl", "jung-algorithms", "jung-visualizati
 COLLECTIONS_GENERIC = "net.sourceforge.collections:collections-generic:jar:4.01"
 COMMONS_MATH = "org.apache.commons:commons-math:jar:2.2"
 AKKA = group("akka-actor", :version=>"1.1.2", :under=>"se.scalablesolutions.akka")
+GRIZZLED = "org.clapper:grizzled-slf4j_#{Scala.version}:jar:0.5"
+SLF4J = "org.slf4j:slf4j-api:jar:1.6.1"
+
+# Test Dependencies
+LOGBACK = group("logback-classic", "logback-core", :under => "ch.qos.logback", :version => "0.9.29")
 
 # Project
 desc "The Retistruen Project"
@@ -32,10 +37,11 @@ define THIS_ARTIFACT do
 
   resources
 
-  compile.with JODA_TIME, JUNG, COLLECTIONS_GENERIC, COMMONS_MATH, AKKA
+  compile.with JODA_TIME, JUNG, COLLECTIONS_GENERIC, COMMONS_MATH, AKKA, GRIZZLED, SLF4J
   compile.using :debug => false
 
   test.resources
+  test.with LOGBACK
   test.using :customscalatest
 
   doc.using :scaladoc
