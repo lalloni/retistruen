7e222941c67f63fefd0b0ba3d26542ede80cdd4b
Pablo Lalloni <plalloni@gmail.com>
2011-07-11 17:21:39 -0300

    Changed version number to 0.4

--- buildfile.rb
+++ buildfile.rb
@@ -4,7 +4,7 @@ require 'buildcustomizations'
 # Project Meta
 THIS_GROUP = "org.retistruen"
 THIS_ARTIFACT = "retistruen"
-THIS_VERSION = "0.4-SNAPSHOT"
+THIS_VERSION = "0.4"
 
 # Repositories
 repositories.remote << "http://www.ibiblio.org/maven2"
