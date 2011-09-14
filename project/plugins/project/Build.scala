3256b055bde6944cf14500778d5add9e2d812ec2
Pablo Lalloni <plalloni@gmail.com>
2011-09-14 00:12:25 -0300

    SbtEclipsify plugin added

---
+++ project/plugins/project/Build.scala
@@ -0,0 +1,7 @@
+import sbt._ 
+import Keys._
+
+object Plugins extends Build {
+  lazy val root = Project("root", file(".")) dependsOn(sbteclipsify) 
+  lazy val sbteclipsify = uri("git://github.com/musk/SbtEclipsify.git")
+}
